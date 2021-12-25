570fb87e28ab8f4f0e032a51fe59c1eafdb6487a
nirzaf <mfmfazrin1986@gmail.com>
2021-12-25 14:46:12 +0530

    Add project files.

---
+++ API/GCCollectController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Atom;
+using Microsoft.SyndicationFeed.Rss;
+using serssly.Data;
+using serssly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace serssly.API
+{
+    [Route("api/gccollect")]
+    [AllowAnonymous]
+    [ApiController]
+    public class GCCollectController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public GCCollectController(ApplicationDbContext context) => this.context = context;
+
+        public ActionResult Get(int gen = 0)
+        {
+            GC.Collect(gen);
+            return Ok();
+        }
+    }
+}
